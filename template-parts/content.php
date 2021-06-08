42d9e0a8225ca2c180309cc258d20319a1aabbd6
Irina <32927392+IriW@users.noreply.github.com>
2021-06-09 00:26:13 +0200

    clean up code

---
+++ template-parts/content.php
@@ -0,0 +1,11 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<header class="entry-header">
+    <h1><?php the_title() ?></h1>
+    <div class="entry-content">
+        <?php the_content() ?>
+    </div>
+
+
+</header>
+</article>
